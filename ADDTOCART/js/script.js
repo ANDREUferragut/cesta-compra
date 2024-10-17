5e18b96e0376497afc77318ad633c5733b097c7a
ANDREUferragut <aferragut19499@iesjoanramis.org>
2024-10-17 08:52:45 +0200

    Add files via upload

--- ADDTOCART/js/script.js
+++ ADDTOCART/js/script.js
@@ -1,6 +1,20 @@
+import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import {getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
 const input = document.getElementById("inputFiled");
 const boto = document.getElementById("afegir");
 
+const appSettings = {
+    databaseURL: "https://lista-de-tareas-andreu-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings);
+const baseDades = getDatabase(app);
+const tasks = ref(baseDades, "tareas")
+
+
 boto.addEventListener("click", function(){
-    console.log(input.value) 
+    push(tasks, input.value)
+    alert("Afegir a la BD") 
 })
+
